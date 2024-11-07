abd274cf99b640e29f58a4791c7c7c5bae784149
joao-menezes1 <joao.menezes@academico.ifpb.edu.br>
2024-11-07 20:30:27 -0300

    teste 2

--- src/main/java/com/securitas/contratae/ContrataeApplication.java
+++ src/main/java/com/securitas/contratae/ContrataeApplication.java
@@ -9,6 +9,7 @@ public class ContrataeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContrataeApplication.class, args);
 		System.out.println("ola");
+		System.out.println("teste2");
 	}
 
 }
