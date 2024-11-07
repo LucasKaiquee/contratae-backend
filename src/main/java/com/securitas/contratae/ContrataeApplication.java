941c4526c4a86a31f106903918ff6a78421a2879
LucasKaiquee <lucaskaique743@gmail.com>
2024-11-07 19:54:19 -0300

    initial commit

---
+++ src/main/java/com/securitas/contratae/ContrataeApplication.java
@@ -0,0 +1,13 @@
+package com.securitas.contratae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrataeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrataeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securitas/contratae/ContrataeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitas.contratae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContrataeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
